5354314ae7c3bfc278ccf54653f042863b489ca8
Khaildoran <sgermscheid@gmail.com>
2020-10-07 15:10:52 -0500

    added post request functionionality

--- server/server.js
+++ server/server.js
@@ -1,34 +1,40 @@
-const express = require('express');
+const express = require('express'); // require express dependecy into server
+const bodyParser = require('body-parser'); // require body parser into server
 
-const app = express();
-const port = 5000;
+const app = express(); //define app
+const port = 5000; //define network port
 
 // get client site files
-app.use(express.static('./server/public'));
+app.use(express.static('./server/public')); // initialize public 
 
+app.use(bodyParser.urlencoded({extended: true}));
 
-let quotesData = [
-    { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
-    { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
-    { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
-];
+const quotesData = require('./modules/quotes');
 
-let index = 0;
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 app.get('/quotes', (req, res) => {
-    console.log('hi from get request');
-    res.send(quotesData);
+
+    res.send(quotesData.list);
 });
 
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-  }
+
 
 app.get('/randomquotes', (req, res) => {
- 
-   let num =  getRandomArbitrary(0, quotesData.length);
-    res.send(quotesData[num]);
+    res.send(quotesData.list[getRandomInt(0, quotesData.list.length -1)]);
 });
+
+app.post('/quotes', (req, res) =>{
+    //
+    quotesData.list.push(req.body)
+    console.log(quotesData);
+    res.sendStatus(200);
+})
+
 
 app.listen(port, () => {
     console.log('Server loaded on port:', port);
