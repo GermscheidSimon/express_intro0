72a41d53cff9c6b081d84746e8dcb2ef7ae8625f
Khaildoran <sgermscheid@gmail.com>
2020-10-07 16:17:36 -0500

    lecture completed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,27 @@
+
+
 console.log('hello from JS');
 
 $(document).ready(onReady)
 
 function onReady() {
     console.log('hellow from JQ');
-    
+    // perform GET request from server
+    getRandomQuote();
 }
+
+function getRandomQuote(){
+    console.log('retrieve data request dispatched');
+    $.ajax({
+        method: 'GET',
+        url: '/randomquotes'
+    }).then( (response) => {
+        console.log(response);
+       appendToDOM(response)
+    });
+}
+
+function appendToDOM(res) {
+    $('#output').append(`${res.quote} by ${res.author}`);
+}
+
